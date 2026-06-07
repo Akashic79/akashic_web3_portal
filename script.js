59a51625d4d41dc7130a860506350e82277f580a
Akashic79 <155839924+Akashic79@users.noreply.github.com>
2026-06-07 23:50:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-</> javaScript
-
-
-const connectButton = document.getElementById("connectButton");
-const walletAddress = document.getElementById("walletAddress");
-
 async function connectWallet() {
 
-    if (typeof window.ethereum !== "undefined") {
-
-        try {
-
-            const accounts = await ethereum.request({
-                method: "eth_requestAccounts"
-            });
-
-            walletAddress.innerHTML =
-                "Connected Wallet:<br>" + accounts[0];
-
-        }
-
-        catch (error) {
-
-            console.log(error);
-
-        }
-
+    if (!window.ethereum) {
+        alert("Please install MetaMask");
+        return;
     }
 
-    else {
+    const accounts = await ethereum.request({
+        method: "eth_requestAccounts"
+    });
 
-        alert("MetaMask is not installed.");
-
-    }
-
+    document.getElementById("walletAddress").innerText =
+        accounts[0];
 }
 
-connectButton.addEventListener("click", connectWallet);
+document
+.getElementById("connectButton")
+.addEventListener("click", connectWallet);
