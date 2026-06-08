a8c952e2fc82b6d8e3d60ad17ac61207cc2f3e82
Akashic79 <155839924+Akashic79@users.noreply.github.com>
2026-06-08 17:07:23 +0700

    Refactor AkashicToken to remove minter functionality
    Removed ERC20Burnable functionality and minter management.

--- contracts/Akashictoken.sol
+++ contracts/Akashictoken.sol
@@ -2,55 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AkashicToken is ERC20, ERC20Burnable, Ownable {
 
-    mapping(address => bool) public minters;
 
-    event MinterAdded(address indexed account);
-    event MinterRemoved(address indexed account);
+contract AkashicToken is ERC20, Ownable {
 
-    constructor(
-        uint256 initialSupply
-    )
+    constructor(uint256 initialSupply)
         ERC20("Akashic", "AKA")
         Ownable(msg.sender)
     {
         _mint(msg.sender, initialSupply * 10 ** decimals());
-
-        minters[msg.sender] = true;
-    }
-
-    modifier onlyMinter() {
-        require(
-            minters[msg.sender],
-            "Not authorized minter"
-        );
-        _;
-    }
-
-    function addMinter(
-        address account
-    )
-        external
-        onlyOwner
-    {
-        minters[account] = true;
-
-        emit MinterAdded(account);
-    }
-
-    function removeMinter(
-        address account
-    )
-        external
-        onlyOwner
-    {
-        minters[account] = false;
-
-        emit MinterRemoved(account);
     }
 
     function mint(
@@ -58,18 +20,8 @@ contract AkashicToken is ERC20, ERC20Burnable, Ownable {
         uint256 amount
     )
         external
-        onlyMinter
+        onlyOwner
     {
         _mint(to, amount);
-    }
-
-    function isMinter(
-        address account
-    )
-        external
-        view
-        returns (bool)
-    {
-        return minters[account];
     }
 }
