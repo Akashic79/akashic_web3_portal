1670caee5f4b98a7171f0a4ecff578f8039b48b3
Akashic79 <155839924+Akashic79@users.noreply.github.com>
2026-06-08 17:04:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const CONTRACT_ADDRESS =
-"0x962D4Cf024F687e5f04176ead33f461D371a7A0F";
+"0x12Fc16A532b463AEfb23A7d9F3F97D275F4c36d0";
 
 const ABI = [
 
-"function balanceOf(address) view returns (uint256)",
-
-"function totalSupply() view returns (uint256)",
-
-"function symbol() view returns (string)",
-
-"function decimals() view returns (uint8)"
+"function name() view returns(string)",
+"function symbol() view returns(string)",
+"function decimals() view returns(uint8)",
+"function totalSupply() view returns(uint256)",
+"function balanceOf(address) view returns(uint256)",
+"function owner() view returns(address)"
 
 ];
 
@@ -17,73 +16,89 @@ let provider;
 let signer;
 let contract;
 
-async function connectWallet() {
+async function connectWallet(){
 
-    if (!window.ethereum) {
+if(!window.ethereum){
 
-        alert("Please install MetaMask");
+alert("Install MetaMask");
 
-        return;
-    }
+return;
 
-    provider = new ethers.providers.Web3Provider(
-        window.ethereum
-    );
-
-    await provider.send(
-        "eth_requestAccounts",
-        []
-    );
-
-    signer = provider.getSigner();
-
-    const address =
-        await signer.getAddress();
-
-    document.getElementById(
-        "walletAddress"
-    ).innerText = address;
-
-    contract = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        ABI,
-        provider
-    );
-
-    await loadTokenData(address);
 }
 
-async function loadTokenData(address) {
+provider =
+new ethers.providers.Web3Provider(
+window.ethereum
+);
 
-    const decimals =
-        await contract.decimals();
+await provider.send(
+"eth_requestAccounts",
+[]
+);
 
-    const balance =
-        await contract.balanceOf(address);
+signer =
+provider.getSigner();
 
-    document.getElementById(
-        "tokenBalance"
-    ).innerText =
-        ethers.utils.formatUnits(
-            balance,
-            decimals
-        );
+const address =
+await signer.getAddress();
 
-    const supply =
-        await contract.totalSupply();
+document.getElementById(
+"walletAddress"
+).innerText = address;
 
-    document.getElementById(
-        "totalSupply"
-    ).innerText =
-        ethers.utils.formatUnits(
-            supply,
-            decimals
-        );
+contract =
+new ethers.Contract(
+CONTRACT_ADDRESS,
+ABI,
+provider
+);
+
+loadData(address);
+
+}
+
+async function loadData(address){
+
+const decimals =
+await contract.decimals();
+
+const balance =
+await contract.balanceOf(address);
+
+document.getElementById(
+"tokenBalance"
+).innerText =
+ethers.utils.formatUnits(
+balance,
+decimals
+);
+
+const supply =
+await contract.totalSupply();
+
+document.getElementById(
+"totalSupply"
+).innerText =
+ethers.utils.formatUnits(
+supply,
+decimals
+);
+
+const owner =
+await contract.owner();
+
+document.getElementById(
+"ownerAddress"
+).innerText =
+owner;
+
 }
 
 document
-.getElementById("connectButton")
+.getElementById(
+"connectButton"
+)
 .addEventListener(
-    "click",
-    connectWallet
+"click",
+connectWallet
 );
